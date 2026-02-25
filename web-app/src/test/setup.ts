26313b6e523b9ddc1445f724d3b16ecaa955210b
Cesar Flores <dev@esauflores.com>
2026-02-25 20:01:47 +0700

    test: add Vitest setup and store tests

---
+++ web-app/src/test/setup.ts
@@ -0,0 +1,11 @@
+// Mock localStorage for Zustand persist
+const localStorageMock = {
+  getItem: () => null,
+  setItem: () => {},
+  removeItem: () => {},
+  clear: () => {},
+  length: 0,
+  key: () => null,
+}
+
+globalThis.localStorage = localStorageMock as Storage
